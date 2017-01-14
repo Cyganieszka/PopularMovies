a44650879be2e2994c5f515c8181890f9010bcc2
cyganieszka <agnieszka.koczuba@gmail.com>
2017-01-14 13:25:31 +0100

    Initial commit

---
+++ app/src/main/java/com/koczuba/popularmovies/PosterGridActivity.java
@@ -0,0 +1,13 @@
+package com.koczuba.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PosterGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poster_grid);
+    }
+}
